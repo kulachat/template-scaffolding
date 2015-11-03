6b08664768af50bf867dcf60d8ef09e37f504281
gigkokman <kann.kupp@gmail.com>
2015-11-03 21:56:55 +0700

    Init project

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Scaffolding Template</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="./css/style.css" rel="stylesheet" />
+</head>
+<body>
+<h1>Scaffolding</h1>
+</body>
+<html>
